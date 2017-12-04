edfd2b2c5d89a788f42b8956038526a8388f981b
Lucas Rigo <lucas.rigo@sap.com>
2017-12-04 21:34:09 -0200

    implemented day4 part 2

--- day4/passphrase.js
+++ day4/passphrase.js
@@ -1,10 +1,12 @@
 var input = [
-  'aa bb cc dd ee',
-  'aa bb cc dd aa',
-  'aa bb cc dd aaa',
  `aa bb cc dd ee
  aa bb cc dd aa
  aa bb cc dd aaa`,
+ `abcde fghij
+ abcde xyz ecdab
+ a ab abc abd abf abj
+ iiii oiii ooii oooi oooo
+ oiii ioii iioi iiio`,
   puzzleInput
 ]
 
@@ -44,11 +46,32 @@ var day4 = function() {
 var day4Part2 = function () {
 
   for (var i = 0; i < input.length; i++) {
-
+    var lines = input[i].split(/\n/)
+    var validPhrases = 0
+    for (var l = 0; l < lines.length; l++) {
+      var words = lines[l].split(/\s/)
+      var dict = {}
+      var size = Object.keys(dict).length
+      var valid = true
+      for (var w = 0; w < words.length; w++) {
+        var sortedWord = words[w].split('').sort().join('')
+        dict[sortedWord] = true
+        var newSize = Object.keys(dict).length
+        if (newSize === size) {
+          valid = false
+          break
+        } else {
+          size = newSize
+        }
+      }
+      if (valid) {
+        validPhrases++
+      }
+    }
 
     $('#part2').append(input[i])
       .append('<br>&emsp;')
-      .append()
+      .append(validPhrases)
       .append('<br>')
   }
 
