8f34a9957e3e192d15a974200784d8d601286679
Lucas Rigo <lucas.rigo@sap.com>
2017-12-04 15:45:16 -0200

    implemented day2 part1

---
+++ day2/checksum.js
@@ -0,0 +1,57 @@
+var input = [
+`5 1 9 5
+7 5 3
+2 4 6 8`,
+  puzzleInput
+]
+
+var day2 = function() {
+
+  for (var i = 0; i < input.length; i++) {
+    var sum = 0
+    var spreadsheet = input[i]
+    var lines = spreadsheet.split('\n')
+    for (var l = 0; l < lines.length; l++) {
+      var numbers = lines[l].split(/\s+/)
+      var min = Number.MAX_SAFE_INTEGER
+      var max = 0
+      for (var n = 0; n < numbers.length; n++) {
+        var num = Number(numbers[n])
+        if (num > max) {
+          max = num
+        }
+        if (num < min) {
+          min = num
+        }
+      }
+      var diff = max - min
+      sum += diff
+    }
+
+    // console.log(sum)
+    $('#day2').append(input[i])
+      .append('<br>&emsp;')
+      .append(sum)
+      .append('<br>')
+  }
+}
+
+var day2Part2 = function () {
+
+  for (var i = 0; i < input.length; i++) {
+    // console.log(sum)
+    $('#part2').append(input[i])
+      .append('<br>&emsp;')
+      .append()
+      .append('<br>')
+  }
+
+}
+
+$(function (){
+  $('#main').append('<div id="day2"><h2>day #1</h2></div>')
+  day2()
+  $('#main').append('<br><div id="part2"><h2>part 2</h2></div>')
+  day2Part2()
+  $('#main').append('<br>')
+})
