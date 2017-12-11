dc5804f8bc3daaf209c49ceb4c8c049cf12f84f6
Lucas Rigo <lucas.rigo@sap.com>
2017-12-11 12:37:29 -0200

    implemented day 9 part 2

--- day9/stream.js
+++ day9/stream.js
@@ -7,7 +7,14 @@ var input = [
 '{{<ab>},{<ab>},{<ab>},{<ab>}}',
 '{{<!!>},{<!!>},{<!!>},{<!!>}}',
 '{{<a!>},{<a!>},{<a!>},{<ab>}}',
-'{{!}!{}}'
+'{{!}!{}}',
+'<>',
+'<random characters>',
+'<<<<>',
+'<{!>}>',
+'<!!>',
+'<!!!>>',
+'<{o"i!a,<{i<a>'
 
 ,puzzleInput
 ]
@@ -52,10 +59,35 @@ var day9 = function() {
 var day9Part2 = function () {
 
   for (var i = 0; i < input.length; i++) {
+    var count = 0
+    var score = 0
+    var str = input[i]
+    var idx = 0
+    var level = 1
+    var garbage = false
+    while (idx < str.length) {
+      var ch = str[idx]
+      if (ch === '!') {
+        idx++
+      } else if (ch === '>') {
+        garbage = false
+      } else if (garbage) {
+        // garbage
+        count++
+      } else if (ch === '<') {
+        garbage = true
+      } else if (ch === '{') {
+        score += level
+        level++
+      } else if (ch === '}') {
+        level--
+      }
+      idx++
+    }
 
     $('#part2').append(input[i])
       .append('<br>&emsp;')
-      .append()
+      .append(count)
       .append('<br>')
   }
 
