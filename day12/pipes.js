33647904246b8f7e820653db66aa32e6d534d770
Lucas Rigo <lucas.rigo@sap.com>
2017-12-12 10:17:08 -0200

    implemented day 12 part 2

--- day12/pipes.js
+++ day12/pipes.js
@@ -30,7 +30,7 @@ var day12 = function() {
       }
       nodes[src] = node
     }
-    console.log(nodes)
+    // console.log(nodes)
 
     var zeroCount = countZeroLinks()
 
@@ -44,7 +44,7 @@ var day12 = function() {
 var countZeroLinks = function() {
   var history = {}
   countLinks(nodes['0'], history)
-  console.log(history)
+  // console.log(history)
   return Object.keys(history).length
 }
 
@@ -57,13 +57,49 @@ var countLinks = function(node, hist) {
   }
 }
 
+
 var day12Part2 = function () {
 
   for (var i = 0; i < input.length; i++) {
 
+    nodes = {}
+    var lines = input[i].split(/\n/)
+    for (var l = 0; l < lines.length; l++) {
+      var parts = lines[l].split(/\s/)
+      var src = parts[0]
+      var node = {
+        'n': src,
+        'links': []
+      }
+      for (var p = 2; p < parts.length; p++) {
+        var n = parts[p].replace(',','')
+        node.links.push(n)
+      }
+      nodes[src] = node
+    }
+    // console.log(nodes)
+    var groups = 0
+
+    var nodeNames = Object.keys(nodes)
+    var nodesLeft = {}
+    $.each(nodeNames, (idx, n) => {
+      nodesLeft[n] = true
+    })
+    $.each(nodeNames, (idx, n) => {
+      if (nodesLeft[n]) {
+        var history = {}
+        countLinks(nodes[n], history)
+        $.each(Object.keys(history), (hidx, hn) => {
+          nodesLeft[hn] = false
+        })
+        groups++
+      }
+    })
+
+
     $('#part2').append(input[i])
       .append('<br>&emsp;')
-      .append()
+      .append(groups)
       .append('<br>')
   }
 
@@ -76,48 +112,3 @@ $(function (){
   day12Part2()
   $('#main').append('<br>')
 })
-
-/*
-
-
-    graphs = []
-    var lines = input[i].split(/\n/)
-    for (var l = 0; l < lines.length; l++) {
-      var parts = lines[l].split(/\s/)
-      var src = parts[0]
-      var idx = inGraphs(src)
-      var g
-      if (idx >= 0) {
-        g = graphs[idx]
-      } else {
-        g = {}
-        g[src] = true
-        graphs.push(g)
-      }
-      for (var p = 2; p < parts.length; p++) {
-        var n = parts[p].replace(',','')
-        var nidx = inGraphs(n)
-        if (nidx >=0) {
-          graphs[nidx][n] = true
-        } else {
-
-        }
-      }
-    }
-    console.log(graphs)
-
-    var zeroIdx = inGraphs('0')
-    var zeroCount = Object.keys(graphs[zeroIdx]).length
-
-
-var inGraphs = function(node) {
-  var graphIdx = -1
-  for (var g = 0; g < graphs.length; g++) {
-    if (graphs[g][node] !== undefined) {
-      graphIdx = g
-      break
-    }
-  }
-  return graphIdx
-}
-*/
