a96df2e0aa75aa0c94e5b3cb891f74a4535d765d
Lucas Magrini Rigo <lucas@magrinirigo.com>
2017-12-17 17:56:24 -0200

    implemented day 17 part 1

---
+++ day17/spinlock.js
@@ -0,0 +1,47 @@
+var input = [
+'3'
+,puzzleInput
+]
+
+var day17 = function() {
+
+  for (var i = 0; i < input.length; i++) {
+    var buffer = [0]
+    var pos = 0
+    var steps = Number(input[i])
+    var spins = 2018
+    for (var j = 1; j < spins; j++) {
+      pos = (pos + steps) % buffer.length
+      buffer.splice(pos+1,0,j)
+      pos++
+    }
+    //console.log(buffer)
+    var idx = buffer.indexOf(2017)
+    // console.log(buffer[idx-3], buffer[idx-2], buffer[idx-1], buffer[idx], buffer[idx+1], buffer[idx+2], buffer[idx+3])
+    var answer = buffer[idx+1]
+
+    $('#day17').append(input[i])
+      .append('<br>&emsp;')
+      .append(answer)
+      .append('<br>')
+  }
+}
+
+var day17Part2 = function () {
+
+  for (var i = 1; i < input.length; i++) {
+    
+    $('#part2').append(input[i])
+      .append('<br>&emsp;')
+      .append()
+      .append('<br>')
+  }
+}
+
+$(function (){
+  $('#main').append('<div id="day17"><h2>day #17</h2></div>')
+  day17()
+  $('#main').append('<br><div id="part2"><h2>part 2</h2></div>')
+  day17Part2()
+  $('#main').append('<br>')
+})
