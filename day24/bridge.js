3df96a5ea1bb1b1d88f2bdd32700317bc40af88e
Lucas Magrini Rigo <lucas@magrinirigo.com>
2017-12-26 14:42:28 -0200

    implemented day 24 part 2

--- day24/bridge.js
+++ day24/bridge.js
@@ -90,7 +90,6 @@ var day24 = function() {
       }
       nextStates.push(...genNextStates(state))
     }
-    //> que 1701
 
     $('#day24').append(input[i])
       .append('<br>&emsp;')
@@ -102,10 +101,88 @@ var day24 = function() {
 var day24Part2 = function () {
 
   for (var i = 0; i < input.length; i++) {
-    
+    var components = []
+    var lines = input[i].split(/\n/)
+    $.each(lines, (idx, val) => {
+      var ports = val.split(/\//)
+      components.push({p1: Number(ports[0]), p2: Number(ports[1])})
+    })
+
+    var genNextStates = function(state) {
+      var nextStates = []
+      $.each(components, (idx, val) => {
+        if(state.idxsUsed.includes(idx)) {
+          return true
+        } else {
+          if (val.p1 === state.nextPort) {
+            var newState = {
+              nextPort: val.p2,
+              sum: state.sum + (val.p1 + val.p2),
+              idxsUsed: state.idxsUsed.slice()
+            }
+            newState.idxsUsed.push(idx)
+            nextStates.push(newState)
+          } else if (val.p2 === state.nextPort) {
+            var newState = {
+              nextPort: val.p1,
+              sum: state.sum + (val.p1 + val.p2),
+              idxsUsed: state.idxsUsed.slice()
+            }
+            newState.idxsUsed.push(idx)
+            nextStates.push(newState)
+          }
+        }
+      }) 
+      return nextStates
+    }
+
+    // find init components
+    var initComps = components.filter((x) => {
+      return x.p1 === 0 || x.p2 === 0
+    })
+
+    var nextStates = []
+    $.each(initComps, (idx, val) => {
+      if (val.p1 === 0) {
+        nextStates.push({
+          nextPort: val.p2,
+          sum: (val.p1 + val.p2),
+          idxsUsed: [components.indexOf(val)]
+        })
+      } else {
+        nextStates.push({
+          nextPort: val.p1,
+          sum: (val.p1 + val.p2),
+          idxsUsed: [components.indexOf(val)]
+        })
+      }
+    })
+    // console.log(nextStates)
+    // console.log(components)
+
+    var maxStrength = -1
+    var maxSize = -1
+    var steps = 0
+    while (nextStates.length > 0) {
+      // shift too slow
+
+      // if (steps++ % 10000 === 0) {
+      //   console.log(steps, maxSize, maxStrength)
+      // }
+      var state = nextStates.pop()
+      if (state.idxsUsed.length > maxSize) {
+        maxSize = state.idxsUsed.length
+        maxStrength = state.sum
+      } else if (state.idxsUsed.length === maxSize
+                  && state.sum > maxStrength) {
+        maxStrength = state.sum
+      }
+      nextStates.push(...genNextStates(state))
+    }
+
     $('#part2').append(input[i])
       .append('<br>&emsp;')
-      .append()
+      .append(maxStrength)
       .append('<br>')
   }
 }
